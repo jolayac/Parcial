22441ffe6af3f3785c7c879c510a34c14c12b959
Juan_Olaya <jolayac@unal.edu.co>
2025-10-09 17:52:26 -0500

    Corrección en clase
    Co-authored-by: JPaezGu <JPaezGu@users.noreply.github.com>

---
+++ biblioteca.py
@@ -0,0 +1,85 @@
+from usuarios import Usuario
+from libros import Libro
+
+
+class Biblioteca:
+    def __init__(self, nombre):
+        self.nombre_bibliotecario = nombre
+        self.lista_usuarios = []
+        self.lista_libros = []
+
+    def registrar_usuario(self):
+        nombre = input("\nIngrese su nombre:\n>").strip(
+        ).title()   # Pide nombre
+        documento = input("\nIngrese su número de documento:\n>").strip(
+        ).title()   # Pide número de documento
+        usuario = Usuario(nombre, documento)    # Crea el usuario
+        self.lista_usuarios.append(usuario)
+        print(f"{'-' * 30}\nUsuario creado\nNombre: {usuario.nombre}\nDocumento: {usuario.documento}\n"
+
+    def registrar_libro(self):
+        # Pedir el título:
+        titulo=input("\nTítulo del libro:\n>").strip().title()
+        # Pedir el autor
+        autor=input("\nAutor del libro:\n>").strip().title()
+        # Para verificar la categoria:
+        categoria_input=input(
+            "Escriba la caegoría del libro. Las opciones son:\n1. Romance\n2. Comedia\n3. Historia\n>")
+        while True:
+            # Transforma el resultado del usuario en minusculas
+            categoria=categoria_input.strip().lower()
+            if categoria == "terror" or categoria == "1":
+                categoria_valida="Terror"
+                break
+            elif categoria == "comedia" or categoria == "2":
+                categoria_valida="Comedia"
+                break
+            elif categoria == "historia" or categoria == "3":
+                categoria_valida="Historia"
+                break  # Dependiendo de la opción, se categoriza el libro
+            else:
+                categoria_input=input(  # Señala que la opción elegida por el usuario es incorrecta o inexistente
+                    f"\n\"{categoria_input}\" No es una opción válida.\nIntente de nuevo.\n>")
+        libro=Libro(titulo, autor, categoria_valida)
+        self.lista_libros.append(libro)
+        print(f"{'-' * 30}\Libro creado\nTítulo: {libro.titulo}\nAutor: {libro.autor}\n"
+
+
+    def mostrar_usuarios(self):
+        for usu in self.lista_usuarios:
+            print(f"\n{usu.nombre}\n{usu.documento}\n")
+
+    def mostrar_libros(self):
+        for lib in self.lista_libros:
+            print(f"\n{lib.nombre}\n{lib.autor}\n{lib.categoria}\n")
+
+    def mostrar_menu(self):
+        while True:
+            seleccion=input(
+                "\nSeleccione lo que quiere hacer (escriba el número de la opción que quiere seleccionar):\n1. Registrar un nuevo usuario.\n2. Registrar un libro.\n3. Mostrar usuarios.\n4. Mostrar libros.\n>").strip()
+            if seleccion == "1":    # Registrar un nuevo usuario.
+                self.registrar_usuario()
+            elif seleccion == "2":  # Registrar un libro.
+                self.registrar_libro()
+            elif seleccion == "3":  # Mostrar usuarios.
+                self.mostrar_usuarios()
+            elif seleccion == "4":  # Mostrar libros.
+                self.mostrar_libros()
+            else:
+                seleccion=input(
+                    f"\n\"{seleccion}\" no está entre las opciones (1 o 2).\nIntente de nuevo.\n>")
+
+
+def registrar_bibliotecario():
+    nombre=input("\nIngrese su nombre\n>").strip().title()
+    bibliotecario=Biblioteca(nombre)
+    return bibliotecario
+
+
+def main():
+    bibliotecario=registrar_bibliotecario()
+    bibliotecario.mostrar_menu()
+
+
+if __name__ == "__main__":
+    main()
--- libros.py
+++ libros.py
@@ -6,29 +6,3 @@ class Libro:
         self.autor = autor
         self.categoria = categoria
 
-
-def registrar_libro():
-    # Pedir el título:
-    titulo = input("\nTítulo del libro:\n>").strip().title()
-    # Pedir el autor
-    autor = input("\nAutor del libro:\n>").strip().title()
-    # Para verificar la categoria:
-    categoria_input = input(
-        "Escriba la caegoría del libro. Las opciones son:\n1. Romance\n2. Comedia\n3. Historia\n>")
-    while True:
-        categoria = categoria_input.strip().lower()
-        if categoria == "terror" or categoria == "1":
-            categoria_valida = "Terror"
-            break
-        elif categoria == "comedia" or categoria == "2":
-            categoria_valida = "Comedia"
-            break
-        elif categoria == "historia" or categoria == "3":
-            categoria_valida = "Historia"
-            break
-        else:
-            categoria_input = input(
-                f"\n\"{categoria_input}\" no es una opción válida.\nIntete de nuevo.\n>")
-
-    libro = Libro(titulo, autor, categoria_valida)
-    return libro
